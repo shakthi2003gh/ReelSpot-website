ff4538eb415c41497b561f736754df37455c3a2b
shakthi <shakthikumar123456789@gmail.com>
2023-11-29 14:27:18 +0530

    Create header layout

--- frontend/src/layouts/header.jsx
+++ frontend/src/layouts/header.jsx
@@ -1,3 +1,26 @@
+import { Link } from "react-router-dom";
+import { MdMenu } from "react-icons/md";
+import useMediaQuery from "../hooks/useMediaQuery";
+
 export default function Header() {
-  return <div>Header</div>;
+  const theme = "light";
+  const isNotMobileDevice = useMediaQuery(450);
+
+  return (
+    <header>
+      <div className="container">
+        <div className="group">
+          {isNotMobileDevice && <MdMenu />}
+
+          <Link to="/" className="logo">
+            <img src={`/logo-${theme}.svg`} alt="Home" />
+          </Link>
+        </div>
+
+        <Link className="btn btn--primary" to="/auth?method=signin">
+          Login
+        </Link>
+      </div>
+    </header>
+  );
 }
