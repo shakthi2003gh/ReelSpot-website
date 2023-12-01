7f2d19134bcc9d5b8db0ac6e711c29f5e5fd01fa
shakthi <shakthikumar123456789@gmail.com>
2023-12-01 07:58:32 +0530

    Create 404 page

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+import { TbError404 } from "react-icons/tb";
+
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found">
+      <TbError404 />
+      <h1>Page Not Found</h1>
+
+      <Link to="/" className="btn btn--primary">
+        go to homepage
+      </Link>
+    </div>
+  );
 }
