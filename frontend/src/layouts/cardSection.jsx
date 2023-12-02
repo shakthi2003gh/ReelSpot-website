1e8be2d450928fcf4f7a783bf09127a7897f4272
shakthi <shakthikumar123456789@gmail.com>
2023-12-02 15:18:39 +0530

    Create card section layout

---
+++ frontend/src/layouts/cardSection.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+import Card from "./../components/card";
+
+export default function CardSection({ title, to, data }) {
+  return (
+    <section className="card-section">
+      <div className="header">
+        {to ? (
+          <>
+            <Link to={to} tabIndex={-1}>
+              <h2>{title || "section"}</h2>
+            </Link>
+
+            <Link to={to} className="view-all">
+              view all <IoIosArrowForward />
+            </Link>
+          </>
+        ) : (
+          <h2>{title}</h2>
+        )}
+      </div>
+      <div className="cards">
+        {data.map(({ id, ...props }) => (
+          <Card key={id} id={id} {...props} />
+        ))}
+      </div>
+    </section>
+  );
+}
