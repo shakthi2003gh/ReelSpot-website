c55110f588d9f53552a4e07fdb9e5c9683d58d54
shakthi <shakthikumar123456789@gmail.com>
2023-12-12 21:45:53 +0530

    Create fetchData helper

--- backend/helper/common.js
+++ backend/helper/common.js
@@ -1,3 +1,5 @@
+const { Provider: http } = require("../http/provider");
+
 function cacheData(Model) {
   return async function (data) {
     const isDataExist = await Model.findOne({ tmdb_id: data.tmdb_id });
@@ -9,3 +11,21 @@ function cacheData(Model) {
     return cachedData;
   };
 }
+
+function fetchData(endpoint, Model, transform) {
+  return async function (tmdb_id) {
+    const isDataExist = await Model.findOne({ tmdb_id });
+    if (isDataExist) return isDataExist;
+
+    return new Promise(async (resolve) => {
+      http(endpoint)
+        .then(async ({ data }) => {
+          const transformedData = await transform(data);
+          resolve(transformedData);
+        })
+        .catch(() => resolve(null));
+    });
+  };
+}
+
+module.exports = { fetchData };
