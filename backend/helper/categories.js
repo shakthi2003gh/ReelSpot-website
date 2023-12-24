4c427d38cc851edd0efc67dcbc906c804fc29b69
shakthi <shakthikumar123456789@gmail.com>
2023-12-24 12:11:19 +0530

    Update category helper

--- backend/helper/categories.js
+++ backend/helper/categories.js
@@ -2,6 +2,10 @@ const { Provider: http } = require("../http/provider");
 const { fetchMovie, getCachedMovieId } = require("./movie");
 const { fetchTvShow, getCachedTvShowId } = require("./tvshow");
 
+function findMediaTypeFromEndpoint(url) {
+  return url.includes("movie") ? "movie" : url.includes("tv") ? "tv" : "";
+}
+
 function fetchCategoryData(endpoint) {
   const fetch = endpoint.includes("movie") ? fetchMovie : fetchTvShow;
 
@@ -26,13 +30,28 @@ function populateCategoriesIds(categories) {
   return new Promise(async (resolve) => {
     const promises = Object.keys(categories).map((category) => {
       const endpoint = categories[category];
-      const getCachedId = endpoint.includes("movie")
-        ? getCachedMovieId
-        : getCachedTvShowId;
+      const media = findMediaTypeFromEndpoint(endpoint);
 
       return new Promise(async (resolve) => {
         const { data } = await http(endpoint);
-        const promises = data.results.map(({ id }) => getCachedId(id));
+        const promises = data.results.map(async ({ id, media_type }) => {
+          const mediaType = media_type || media;
+          const getCachedId =
+            mediaType === "movie" ? getCachedMovieId : getCachedTvShowId;
+
+          if (endpoint.includes("/trending/all")) {
+            return new Promise(async (resolve) => {
+              const data = await getCachedId(id);
+
+              resolve({
+                data,
+                mediaType: mediaType === "movie" ? "Movie" : "TvShow",
+              });
+            });
+          }
+
+          return getCachedId(id);
+        });
         const results = await Promise.all(promises);
 
         categoryData[category] = results;
